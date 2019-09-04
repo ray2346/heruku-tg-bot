6fc5e0ab13ed306f27bd329ec04566acf389b5fd
ray2346 <54332835+ray2346@users.noreply.github.com>
2019-09-05 00:31:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 const TelegramBot = require('node-telegram-bot-api')
-    const TOKEN = '882468554:AAH_8_eE4dATkcsVygNY0FuSPqVGMd4Q1Oc'
-    const bot = new TelegramBot (TOKEN, {polling: true} )
+const TOKEN = '882468554:AAH_8_eE4dATkcsVygNY0FuSPqVGMd4Q1Oc'
+const bot = new TelegramBot (TOKEN, {polling: true} )
 
-    bot.on('message', msg =>
-    {bot.sendMessage (msg.chat.id, 'helloy epta yeba)))/start11')})
-bot.on('/start11', msg =>
-    {bot.sendMessage (msg.chat.id, 'helloy epta yeba)))/start11')})
+bot.on('message', msg =>
+{bot.sendMessage (msg.chat.id, 'helloy epta yeba)))/start11')})
 
-    if (msg.text=="/start11") {
 
-        bot.sendMessage(id, "Первый вопрос: ")}
+bot.hears('/start11', (ctx) => ctx.reply('Hey there'))
