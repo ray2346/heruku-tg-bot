6b9b77af30daf65536db471c1efef15d38ec1acb
ray2346 <54332835+ray2346@users.noreply.github.com>
2019-09-04 21:42:10 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,3 @@ const TOKEN = '882468554:AAH_8_eE4dATkcsVygNY0FuSPqVGMd4Q1Oc'
 const bot = new TelegramBot (TOKEN, {polling: true} )
 bot.on('message', msg =>
 {bot.sendMessage (msg.chat.id, 'helloy ve pidor tebe pizda)))')})
-if message.text == "Привет":
-    bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
-elif message.text == "/help":
-    bot.send_message(message.from_user.id, "Напиши привет")
-else:
-    bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
-bot.polling(none_stop=True, interval=0)
