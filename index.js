eb80347320d3ff28b634bb174d70842bad12d79b
ray2346 <tooshka4@yandex.ru>
2019-09-04 21:09:27 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const TelegramBot = require('node-telegram-bot-api')
+const TOKEN = '882468554:AAH_8_eE4dATkcsVygNY0FuSPqVGMd4Q1Oc'
+//C:\Users\Хозяина\WebstormProjects\heruku_bot9
+const bot = new TelegramBot (TOKEN, {polling: true} )
+bot.on('message', msg =>
+{bot.sendMessage (msg.chat.id, 'helloy epta yeba)))')})
