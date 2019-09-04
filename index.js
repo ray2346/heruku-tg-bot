3313f13644e9966f7e710eeeffb51ed8745f3a63
ray2346 <54332835+ray2346@users.noreply.github.com>
2019-09-04 21:44:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
-const TelegramBot = require('node-telegram-bot-api')
-const TOKEN = '882468554:AAH_8_eE4dATkcsVygNY0FuSPqVGMd4Q1Oc'
-//C:\Users\Хозяина\WebstormProjects\heruku_bot9
-const bot = new TelegramBot (TOKEN, {polling: true} )
-bot.on('message', msg =>
-{bot.sendMessage (msg.chat.id, 'helloy ve pidor tebe pizda)))')})
+pip install pytelegrambotapi
+
+import telebot;
+bot = telebot.TeleBot('%ваш токен%');
+//Теперь объявим метод для получения текстовых сообщений:
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+
+@bot.message_handler(content_types=['text', 'document', 'audio'])
+
+if message.text == "Привет":
+    bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+elif message.text == "/help":
+    bot.send_message(message.from_user.id, "Напиши привет")
+else:
+    bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+bot.polling(none_stop=True, interval=0)
+python bot.py
