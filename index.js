6b15970de8a62c09c4b9ca39a8ec6222ba8f2b35
ray2346 <54332835+ray2346@users.noreply.github.com>
2019-09-04 23:33:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,19 +3,12 @@ const TOKEN = '882468554:AAH_8_eE4dATkcsVygNY0FuSPqVGMd4Q1Oc'
 //C:\Users\Хозяина\WebstormProjects\heruku_bot9
 const bot = new TelegramBot (TOKEN, {polling: true} )
 
-
-
-
-
     bot.on('message', msg =>
     {bot.sendMessage (msg.chat.id, 'helloy epta yeba)))/start11')})
 
-    var id=msg.from.id;
-
-    bot.on('message', function(msg){
     if(msg.text=="/start11"){
-        bot.sendMessage(id, 'кнопка перевела вот сюда');}
-    
+        bot.sendMessage(id, 'кнопка перевела вот сюда');
 
 
-});
+
+};
