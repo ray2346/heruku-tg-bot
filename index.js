c3fe42f62a033aa32e07f394f66d907986caace5
ray2346 <54332835+ray2346@users.noreply.github.com>
2019-09-04 22:16:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,8 @@
-var TelegramBot = require('node-telegram-bot-api');
+const TelegramBot = require('node-telegram-bot-api')
+const TOKEN = '882468554:AAH_8_eE4dATkcsVygNY0FuSPqVGMd4Q1Oc'
+//C:\Users\Хозяина\WebstormProjects\heruku_bot9
+const bot = new TelegramBot (TOKEN, {polling: true} )
 
-var token = '882468554:AAH_8_eE4dATkcsVygNY0FuSPqVGMd4Q1Oc';
-var bot = new TelegramBot(token, {polling: true});
 
-var notes = [];
-
-bot.onText(/напомни (.+) в (.+)/, function (msg, match) {
-    var userId = msg.from.id;
-    var text = match[1];
-    var time = match[2];
-
-    notes.push({ 'uid': userId, 'time': time, 'text': text });
-
-    bot.sendMessage(userId, 'Отлично! Я обязательно напомню, если не сдохну :)');
-});
-
-setInterval(function(){
-    for (var i = 0; i < notes.length; i++) {
-    const curDate = new Date().getHours() + ':' + new Date().getMinutes();
-    if (notes[i]['time'] === curDate) {
-      bot.sendMessage(notes[i]['uid'], 'Напоминаю, что вы должны: '+ notes[i]['text'] + ' сейчас.');
-      notes.splice(i, 1);
-    }
-  }
-}, 1000)
+bot.on('message', msg =>
+{bot.sendMessage (msg.chat.id, 'helloy epta yeba)))')})
