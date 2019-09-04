3a2cd9ae2239e424c23556ef837f7833e1f2dd3a
ray2346 <54332835+ray2346@users.noreply.github.com>
2019-09-04 23:43:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,5 +3,16 @@ const TOKEN = '882468554:AAH_8_eE4dATkcsVygNY0FuSPqVGMd4Q1Oc'
 //C:\Users\Хозяина\WebstormProjects\heruku_bot9
 const bot = new TelegramBot (TOKEN, {polling: true} )
 
-    bot.on('message', msg =>
-    {bot.sendMessage (msg.chat.id, 'helloy epta yeba)))/start11')})
+bot.on('message', function (msg) {
+var chatId = msg.chat.id;
+console.log(msg);
+if(msg.text == 'photo')
+fs.exists(filename, function (exists) {
+if(exists)
+fs.unlinkSync(filename);
+imagesnapjs.capture(filename, { cliflags: '-w 2'}, function(err) {
+console.log(err ? err : 'Success!');
+bot.sendPhoto(chatId, filename, {caption: "It's your photo!"});
+});
+});
+});
