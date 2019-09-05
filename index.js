e092aa894072a058fddc0ea828738ee4a3ae5b0f
ray2346 <54332835+ray2346@users.noreply.github.com>
2019-09-05 11:16:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const TelegramBot = require('node-telegram-bot-api')
 const TOKEN = '882468554:AAH_8_eE4dATkcsVygNY0FuSPqVGMd4Q1Oc'
 const bot = new TelegramBot (TOKEN, {polling: true} )
 
-sendMessageByBot(messageChatId, 'helloy epta yeba)))/say')
 bot.on('text', function(msg)
 {
     if (messageText === '/say') {
