b6fed51a7a7a0b40ec041049c8837cfd05765e6f
ray2346 <54332835+ray2346@users.noreply.github.com>
2019-09-05 11:04:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,4 +3,17 @@ const TOKEN = '882468554:AAH_8_eE4dATkcsVygNY0FuSPqVGMd4Q1Oc'
 const bot = new TelegramBot (TOKEN, {polling: true} )
 
 bot.on('message', msg =>
-{bot.sendMessage (msg.chat.id, 'helloy epta yeba)))/start11')})
+{bot.sendMessage (msg.chat.id, 'helloy epta yeba)))/say')})
+bot.on('text', function(msg)
+{
+    var messageChatId = msg.chat.id;
+    var messageText = msg.text;
+    var messageDate = msg.date;
+    var messageUsr = msg.from.username;
+ 
+    if (messageText === '/say') {
+        sendMessageByBot(messageChatId, 'Hello World!');
+    }
+ 
+    console.log(msg);
+});
