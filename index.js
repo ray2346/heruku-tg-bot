3bb991526689cdcc64d386af4fe0cdd709e5e83b
ray2346 <54332835+ray2346@users.noreply.github.com>
2019-09-05 13:22:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,21 @@
 var TelegramBot = require('node-telegram-bot-api');
- 
 var token = '882468554:AAH_8_eE4dATkcsVygNY0FuSPqVGMd4Q1Oc';
 var botOptions = { polling: true};
 var bot = new TelegramBot(token, botOptions);
 var messageText = msg.text;
 
-bot.on('text', function(msg)
-{
-   
- 
-    if (messageText === '/sart') {
-        sendMessageByBot(messageChatId, 'Hello World8784у4у87!/say');
-    }
- 
-    console.log(msg);
-});
- 
-
-
+bot.getMe().then(function(me));
  
 bot.on('text', function(msg)
-{
- 
+{ 
     if (messageText === '/say') {
-        sendMessageByBot(messageChatId, 'Hello World!/say1');
+        sendMessageByBot(messageChatId, 'Hello World!');
     }
  
     console.log(msg);
 });
  
+function sendMessageByBot(aChatId, aMessage)
+{
+    bot.sendMessage(aChatId, aMessage, { caption: 'I\'m a cute bot!' });
+}
