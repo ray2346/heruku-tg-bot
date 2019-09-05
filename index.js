018ebb6baea860afc87c4f5b683bf826a1bf0e75
ray2346 <54332835+ray2346@users.noreply.github.com>
2019-09-05 13:27:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,14 +4,26 @@ var botOptions = { polling: true};
 var bot = new TelegramBot(token, botOptions);
 var messageText = msg.text;
 
-bot.getMe().then(function(me));
+bot.getMe().then(function(me)
+{
+    console.log('Hello! My name is %s!', me.first_name);
+    console.log('My id is %s.', me.id);
+    console.log('And my username is @%s.', me.username);
+});
  
 bot.on('text', function(msg)
-{ 
+{
+    var messageText = msg.text;
+
+ 
     if (messageText === '/say') {
         sendMessageByBot(messageChatId, 'Hello World!');
     }
  
     console.log(msg);
 });
-
+ 
+function sendMessageByBot(aChatId, aMessage)
+{
+    bot.sendMessage(aChatId, aMessage, { caption: 'I\'m a cute bot!' });
+}
