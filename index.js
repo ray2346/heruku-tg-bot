f7749db5e9e48173dbb696267a336027e78479c3
ray2346 <54332835+ray2346@users.noreply.github.com>
2019-09-05 14:47:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
 var TelegramBot = require('node-telegram-bot-api');
 var token = '882468554:AAH_8_eE4dATkcsVygNY0FuSPqVGMd4Q1Oc';
 var botOptions = {polling: true};
-
 var bot = new TelegramBot(token, botOptions);
  
 bot.getMe().then(function(me)
 {
     console.log('Hello! My name is %s!', me.first_name);
+    console.log('My id is %s.', me.id);
+    console.log('And my username is @%s.', me.username);
 });
  
 bot.on('text', function(msg)
 {
-  
+    var messageChatId = msg.chat.id;
     var messageText = msg.text;
+    var messageDate = msg.date;
+    var messageUsr = msg.from.username;
  
     if (messageText === '/say') {
-        sendMessageByBot(messageChatId, 'Hello World!/say2');
+        sendMessageByBot(messageChatId, Hello World\Hello World!/say2');
     }
  
     console.log(msg);
 });
+ 
 function sendMessageByBot(aChatId, aMessage)
 {
     bot.sendMessage(aChatId, aMessage, { caption: 'I\'m a cute bot!' });
@@ -27,19 +31,24 @@ function sendMessageByBot(aChatId, aMessage)
 bot.getMe().then(function(me)
 {
     console.log('Hello! My name is %s!', me.first_name);
+    console.log('My id is %s.', me.id);
+    console.log('And my username is @%s.', me.username);
 });
  
 bot.on('text', function(msg)
 {
-  
+    var messageChatId = msg.chat.id;
     var messageText = msg.text;
+    var messageDate = msg.date;
+    var messageUsr = msg.from.username;
  
-    if (messageText === '/say') {
-        sendMessageByBot(messageChatId, 'Плати деньги\Много плати');
+    if (messageText === '/say2') {
+        sendMessageByBot(messageChatId, 'Hello World!все работаем');
     }
  
     console.log(msg);
 });
+ 
 function sendMessageByBot(aChatId, aMessage)
 {
     bot.sendMessage(aChatId, aMessage, { caption: 'I\'m a cute bot!' });
