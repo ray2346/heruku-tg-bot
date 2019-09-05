7d0fdfe2ee47588dbc77a69e456887f1831f2e98
ray2346 <54332835+ray2346@users.noreply.github.com>
2019-09-05 11:20:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,30 @@ const TelegramBot = require('node-telegram-bot-api')
 const TOKEN = '882468554:AAH_8_eE4dATkcsVygNY0FuSPqVGMd4Q1Oc'
 const bot = new TelegramBot (TOKEN, {polling: true} )
 
+var bot = new TelegramBot(token, botOptions);
+ 
+bot.getMe().then(function(me)
+{
+    console.log('Hello! My name is %s!', me.first_name);
+    console.log('My id is %s.', me.id);
+    console.log('And my username is @%s.', me.username);
+});
+ 
 bot.on('text', function(msg)
 {
+    var messageChatId = msg.chat.id;
+    var messageText = msg.text;
+    var messageDate = msg.date;
+    var messageUsr = msg.from.username;
+ 
     if (messageText === '/say') {
         sendMessageByBot(messageChatId, 'Hello World!');
     }
  
     console.log(msg);
 });
+ 
+function sendMessageByBot(aChatId, aMessage)
+{
+    bot.sendMessage(aChatId, aMessage, { caption: 'I\'m a cute bot!' });
+}
