dcfeb47938266ba29c0c1c76e28f341fa2fb74f3
ray2346 <54332835+ray2346@users.noreply.github.com>
2019-09-06 15:27:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,39 +3,6 @@ var token = '882468554:AAH_8_eE4dATkcsVygNY0FuSPqVGMd4Q1Oc';
 var botOptions = {polling: true};
 var bot = new TelegramBot(token, botOptions);
  
-bot.getMe().then(function(me)
-{
-    console.log('Hello! My name is %s!', me.first_name);
-    console.log('My id is %s.', me.id);
-    console.log('And my username is @%s.', me.username);
-});
- 
-bot.on('text', function(msg)
-{
-    var messageChatId = msg.chat.id;
-    var messageText = msg.text;
-    var messageDate = msg.date;
-    var messageUsr = msg.from.username;
- 
-    if (messageText === '/start') {
-        sendMessageByBot(messageChatId, "Hello World \ Hello World!/say2");
-    }
- 
-    console.log(msg);
-});
- 
-function sendMessageByBot(aChatId, aMessage)
-{
-    bot.sendMessage(aChatId, aMessage, { caption: 'I\'m a cute bot!' });
-}
-var bot = new TelegramBot(token, botOptions);
- 
-bot.getMe().then(function(me)
-{
-    console.log('Hello! My name is %s!', me.first_name);
-    console.log('My id is %s.', me.id);
-    console.log('And my username is @%s.', me.username);
-});
  
 bot.on('text', function(msg)
 {
