abacfe94bea3f0f4553614fe208a5ab7f489ceec
ray2346 <54332835+ray2346@users.noreply.github.com>
2019-09-06 15:43:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ var TelegramBot = require('node-telegram-bot-api');
 var token = '882468554:AAH_8_eE4dATkcsVygNY0FuSPqVGMd4Q1Oc';
 var botOptions = {polling: true};
 var bot = new TelegramBot(token, botOptions);
- bot.on('text', function(msg)
+ var messageChatId = msg.chat.id;
+ var messageText = msg.text;
+bot.on('text', function(msg)
 {
-    var messageChatId = msg.chat.id;
-    var messageText = msg.text;
-    var messageDate = msg.date;
-    var messageUsr = msg.from.username;
+   
+
   
     if (messageText === '/start') 
     {sendMessageByBot(messageChatId, "3 Первая строчка" + '\n' + "Вторая строчка  /say2 ");}
@@ -16,10 +16,7 @@ var bot = new TelegramBot(token, botOptions);
 });
  bot.on('text', function(msg)
 {
-    var messageChatId = msg.chat.id;
-    var messageText = msg.text;
-    var messageDate = msg.date;
-    var messageUsr = msg.from.username;
+  
   
     if (messageText === '/say2') 
     {sendMessageByBot(messageChatId, "4 Первая строчка" + '\n' + "Вторая строчка");}
