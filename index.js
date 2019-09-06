665bf66d25e3b48ab4283878301d4a152a7798d2
ray2346 <54332835+ray2346@users.noreply.github.com>
2019-09-06 15:51:29 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,28 +4,20 @@ var botOptions = {polling: true};
 var bot = new TelegramBot(token, botOptions);
  
 bot.on('text', function(msg)
-{
-   
+{  
  var messageChatId = msg.chat.id;
  var messageText = msg.text;
   
     if (messageText === '/start') 
-    {sendMessageByBot(messageChatId, "3 Первая строчка" + '\n' + "Вторая строчка  /say2 ");}
-     
-  
-});
- bot.on('text', function(msg)
+    {sendMessageByBot(messageChatId, "3 Первая строчка" + '\n' + "Вторая строчка  /say2 ");}});
+
+bot.on('text', function(msg)
 {
+ var messageChatId = msg.chat.id;
+ var messageText = msg.text;
   
     if (messageText === '/say2') 
-    {sendMessageByBot(messageChatId, "4 Первая строчка" + '\n' + "Вторая строчка");}
-    
-  
-});
+    {sendMessageByBot(messageChatId, "4 Первая строчка" + '\n' + "Вторая строчка");}});
 
 
-function sendMessageByBot(aChatId, aMessage)
-{
-    bot.sendMessage(aChatId, aMessage, { caption: 'I\'m a cute bot!' });
-}
 
